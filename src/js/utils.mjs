47d363099f044eed702d17e257ba98623886bab0
preussju <jveiga@byupathway.edu>
2026-06-02 16:44:50 +0200

    creating the initial files

---
+++ src/js/utils.mjs
@@ -0,0 +1,25 @@
+
+// functions used to load the partials 
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadHeaderFooter() {
+const headerTemplate = await loadTemplate("./public/partials/header.html");
+const footerTemplate = await loadTemplate("./public/partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
